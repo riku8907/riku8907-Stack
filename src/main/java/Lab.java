8e0693cb9ab6d0fa79811bb2ce18e2f7e5720a9d
Raul Jauregui <raul.jauregui2@icloud.com>
2023-03-12 05:34:25 +0000

    StackImplemations Lab Completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,4 +1,5 @@
 import java.util.Deque;
+import java.util.LinkedList;
 /**
  * Stacks are LIFO - last in, first out, like a stack of plates or books, or the puzzle game "Towers of Hanoi".
  * They are used in algorithms where it is needed to keep track of the ordering in which the elements were inserted.
@@ -20,7 +21,8 @@ public class Lab {
      * @return a newly instantiated Deque. Deques implement both stack and queue behavior.
      */
     public Deque<String> createNewStack(){
-        return null;
+        Deque<String> stack = new LinkedList<String>();
+        return stack;
     }
 
     /**
@@ -28,7 +30,7 @@ public class Lab {
      * @return the number of elements in the deque.
      */
     public int getSize(Deque<String> stack){
-        return 0;
+        return stack.size();
     }
 
     /**
@@ -38,16 +40,18 @@ public class Lab {
      * @param item a String to be added to the deque.
      */
     public void addToTopOfStack(Deque<String> stack, String item){
+        stack.add(item);
     }
 
     /**
      * Pop the value from the top of the deque and return that value.
      * (this removes the most recently-pushed value)
      * @param stack A Deque of Strings.
+     * @param deque TODO
      * @return the value popped from the top of the deque.
      */
-    public String removeFromTopOfStack(Deque<String> stack){
-        return "";
+    public String removeFromTopOfStack(Deque<String> stack, Deque<String> deque){
+        return stack.poll();
     }
     /**
      * Peek the value at the top of a deque.
@@ -57,6 +61,10 @@ public class Lab {
      * @return the value peeked from the top of the deque.
      */
     public String getTopOfStackWithoutRemoving(Deque<String> stack){
-        return "";
+        return stack.peekLast();
+    }
+
+    public String removeFromTopOfStack(Deque<String> myStack) {
+      return myStack.pollLast();
     }
 }
